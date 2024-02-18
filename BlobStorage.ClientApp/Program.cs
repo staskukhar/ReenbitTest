7cb4a4f0ee65d1716b2950b43ebd14b0ad283af5
staskukhar <staskuk2005@gmail.com>
2024-02-18 16:53:27 +0200

    creates client blazor app

---
+++ BlobStorage.ClientApp/Program.cs
@@ -0,0 +1,13 @@
+using BlobStorage.ClientApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { 
+    BaseAddress = new Uri("base_url_for_api_access") });
+builder.Services.AddBlazorBootstrap();
+
+await builder.Build().RunAsync();
