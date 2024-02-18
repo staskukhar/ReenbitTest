fbb692796726a49feda4a8dd4bd124c4fc3db48d
staskukhar <staskuk2005@gmail.com>
2024-02-18 16:53:10 +0200

    creates shared resource project

---
+++ BlobStorage.Shared/FileExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace BlobStorage.Shared
+{
+    public static class FileExtensions
+    {
+        public const string Docx = ".docx";
+        public const string DocxContentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+    }
+}
---
+++ BlobStorage.Shared/Models/BlobUploadRequestDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BlobStorage.Shared.Models
+{
+    public class BlobUploadRequestDto
+    {
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+        [JsonPropertyName("name")]
+        public string FileName { get; set; }
+        [JsonPropertyName("binary")]
+        public BinaryData BinaryData { get; set; }
+
+        public BlobUploadRequestDto(string email, string fileName, BinaryData binaryData)
+        {
+            Email = email;  
+            FileName = fileName;
+            BinaryData = binaryData;
+        }
+        public BlobUploadRequestDto() { }   
+    }
+}
